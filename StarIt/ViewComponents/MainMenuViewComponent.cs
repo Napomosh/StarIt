4856101d976b456bcf2c71020da6d5ea6fd98cab
Aleksandr Meller <aleksandr.meller.cpp@gmail.com>
2024-11-06 21:42:28 +0500

    Add main menu component

---
+++ StarIt/ViewComponents/MainMenuViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarIt.Bl;
+
+namespace StarIt.ViewComponents;
+
+public class MainMenuViewComponent(ICurrentUser currentUser) : ViewComponent
+{
+    private readonly ICurrentUser currentUser = currentUser;
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        bool isLoggedIn = await currentUser.IsAuthenticated();
+        return View("Index", isLoggedIn);
+    }
+}
